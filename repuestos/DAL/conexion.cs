508a59936bcdf36c2a3ae07542bb3706894829ed
Vini96 <vincebayana@gmail.com>
2020-06-23 20:12:41 +0200

    Commit de conexion

---
+++ repuestos/DAL/conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public class conexion
+    {
+        public SqlConnection conectar()
+        {
+            string sCadenaConexion = " server=DESKTOP-00E9Q34\\VINBA; database=db_repuestos;Integrated Security= True  ";
+            SqlConnection conectar = new SqlConnection();
+
+            try
+            {
+                conectar.ConnectionString = sCadenaConexion;
+                conectar.Open();
+                return conectar;
+
+            }
+            catch (Exception ex)
+            {
+                //Excepcion por si la base de datos no se conecta
+                Console.WriteLine("Error en la conexion a la base de datos" + ex.Message);
+                return null;
+
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ repuestos/Entity/repuestos_entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class repuestos_entity
+    {
+        public int idrepuesto { get; set; }
+        public string nombre_repuesto { get; set; }
+        public string descripcion_repuesto { get; set; }
+        public int precio_unitario { get; set; }
+        public string estado_repuesto { get; set; }
+    }
+
+   
+
+}
